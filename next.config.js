69a638c989b31ec8f3f045cd76bee6df42ca3d6d
sarpkayature <sarpkaya.ture@segmentify.com>
2023-03-27 15:08:37 +0300

    fix: ci

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  distDir: 'dist',
   experimental: {
-    output: 'export',
-    distDir: 'dist',
     appDir: true,
   },
 };
