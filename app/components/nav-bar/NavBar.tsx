3b30d6ecc5de515d51116f044d23d5a6ce58f077
sarpkayature <sarpkayature@gmail.com>
2023-03-28 15:01:13 +0300

    chore: stylelint file updated

--- app/components/nav-bar/NavBar.tsx
+++ app/components/nav-bar/NavBar.tsx
@@ -11,6 +11,7 @@ import Typography from "@mui/material/Typography";
 import Menu from "@mui/material/Menu";
 import MenuIcon from "@mui/icons-material/Menu";
 import Container from "@mui/material/Container";
+
 import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import Tooltip from "@mui/material/Tooltip";
