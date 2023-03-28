c67fe1dd04b749ba02c67883158ad9c7f0ddc23e
sarpkayature <sarpkayature@gmail.com>
2023-03-28 15:06:00 +0300

    fix: file updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,11 @@
 "use client";
 import "./globals.css";
+import { ReactNode } from "react";
 import { Theme } from "./common/config/theme/themes";
 
 import { ThemeProvider, CssBaseline } from "@mui/material";
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <ThemeProvider theme={Theme}>
