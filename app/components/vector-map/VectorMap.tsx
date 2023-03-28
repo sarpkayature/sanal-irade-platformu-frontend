47cdf0ffe4ee3db4a72553c1d599a46109d9d514
sarpkayature <sarpkayature@gmail.com>
2023-03-28 17:07:34 +0300

    chore: change github actions

--- app/components/vector-map/VectorMap.tsx
+++ app/components/vector-map/VectorMap.tsx
@@ -1,12 +1,12 @@
-"use client";
+'use client';
 
-import { VectorMap } from "@south-paw/react-vector-maps";
-import classes from "./VectorMap.module.scss";
-import TurkeyMap from "./constants/Turkey.json";
+import { VectorMap } from '@south-paw/react-vector-maps';
+import classes from './VectorMap.module.scss';
+import TurkeyMap from './constants/Turkey.json';
 
 export default function VectorMaps() {
   return (
-    <div className={classes["map-container"]}>
+    <div className={classes['map-container']}>
       <VectorMap {...TurkeyMap} />
     </div>
   );
